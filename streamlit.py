5aeaf5ea55048a17bf61c62ee2735d49a5fb5f7e
rajeshai <73292655+rajeshai@users.noreply.github.com>
2021-10-17 18:29:21 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,6 +14,7 @@ api = tw.API(auth, wait_on_rate_limit=True)
 classifier = pipeline('sentiment-analysis')
 
 st.title('Live Twitter Sentiment Analysis with Tweepy and HuggingFace Transformers')
+st.markdown('This app uses tweepy to get tweets from twitter based on the input name/phrase. It then processes the tweets through HuggingFace transformers pipeline function for sentiment analysis. The resulting sentiments and corresponding tweets are then put in a dataframe for display which is what you see as result.')
 
 def run():
 
